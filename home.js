be4eeec22157585511c388f709514c9dca18c7d0
Alex van Zijl <hello@alexvanzijl.nl>
2025-12-15 12:00:39 +0100

    Update home.js

--- home.js
+++ home.js
@@ -181,14 +181,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // SERVICE BLOCKS
 
-ScrollTrigger.create({
-  trigger: '.why_services', // full-width section
-  start: 'top top+=32',
-  end: 'bottom bottom',
-  pin: '.service_block_360',
-  pinSpacing: false,
-});
-
+//ScrollTrigger.create({
+//  trigger: '.service_block_360_wrapper',
+//  start: 'top 32px',
+//  end: 'bottom bottom',
+//  pin: '.service_block_360',
+//  pinSpacing: false
+//});
 
 //////////////
 //// LOADER //
