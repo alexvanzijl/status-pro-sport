cefe7842f88713d19c5246e632c2ead8129b80bd
Alex van Zijl <hello@alexvanzijl.nl>
2025-12-22 18:29:21 +0100

    Update profile.js

--- profile.js
+++ profile.js
@@ -39,12 +39,22 @@ ScrollTrigger.matchMedia({
   }
 });
 
-///////////////////
-// HERO PARALLAX //
-///////////////////
+//////////////
+// PARALLAX //
+//////////////
+
+// HERO
 
 initImageParallax({
   containerSelector: '.hero_secondary_details_wrapper',
   imageSelector: '.hero_secondary_details',
+  yPercent: 10
+});
+
+// ADDITIONAL IMGS
+
+initImageParallax({
+  containerSelector: '.news_body_img_wrapper',
+  imageSelector: '.news_body_img',
   yPercent: 10
 });
