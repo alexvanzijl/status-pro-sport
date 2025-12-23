8859dbbde7462a80927a84a8205751ab89746860
Alex van Zijl <hello@alexvanzijl.nl>
2025-12-23 12:32:28 +0100

    Create studio.js

---
+++ studio.js
@@ -0,0 +1,20 @@
+//STUDIO
+console.log ('STUDIO LOADED V1.0');
+
+//////////////
+// PARALLAX //
+//////////////
+
+// HERO
+initImageParallax({
+  containerSelector: '.hero_bg-img_wrapper',
+  imageSelector: '.hero_secondary_details',
+  yPercent: 10
+});
+
+// CALL OUT
+initImageParallax({
+  containerSelector: '.call-out_visual',
+  imageSelector: '.call-out_visual_img',
+  yPercent: 10
+});
