be43d94073d6a8c102c00573f60d888ce0f6806b
Alex van Zijl <hello@alexvanzijl.nl>
2025-12-28 13:11:36 +0100

    Update pdp.js

--- pdp.js
+++ pdp.js
@@ -11,3 +11,41 @@ initImageParallax({
   imageSelector: '.pdp_img',
   yPercent: 10
 });
+
+////////////////
+// STICKY TXT //
+////////////////
+ScrollTrigger.matchMedia({
+
+  // DESKTOP & TABLET
+  "(min-width: 768px)": function () {
+
+    ScrollTrigger.create({
+      trigger: '.pdp_body',
+      start: 'top top+=32',
+
+      end: () => {
+        const grid  = document.querySelector('.pdp_txt_container');
+        const block = document.querySelector('.pdp_txt');
+        const topOffset = 0;
+
+        if (!grid || !block) return '+=0';
+
+        const dist = grid.offsetHeight - block.offsetHeight - topOffset;
+        return `+=${Math.max(0, dist)}`;
+      },
+
+      pin: '.pdp_txt',
+      pinSpacing: false,
+      anticipatePin: 1
+      // markers: true
+    });
+
+  },
+
+  // MOBILE
+  "(max-width: 767px)": function () {
+    // Intentionally empty
+  }
+
+});
