b999f14b9a937c2c453f14afadf0aa54a1ff79df
alexvanzijl <hello@alexvanzijl.nl>
2026-01-13 20:28:33 +0100

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+//PORTFOLIO
+console.log ('PORTFOLIO LOADED V1.0');
+
+//////////////
+// PARALLAX //
+//////////////
+
+// HERO
+initImageParallax({
+  containerSelector: '.hero_video_container',
+  imageSelector: '.hero_video',
+  yPercent: 10
+});
